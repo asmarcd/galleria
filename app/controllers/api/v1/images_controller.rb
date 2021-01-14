d5a6d3c1d1695ee06e06d35a96ed5be6e67796a0
Chris Asmar <asmarcd@gmail.com>
2021-01-13 19:58:05 -0800

    db seeded and confirmed working

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,13 +1,38 @@
 class Api::V1::ImagesController < ApplicationController
   def index
+    image = Image.all.order(created_at: :desc)
+    render json: image
   end
 
   def create
+    image = Image.create!(image_params)
+    if image 
+      render json: image
+    else
+      render json: image.errors
+    end
   end
 
   def show
+    if image
+      render json: image
+    else
+      render json: image.errors
+    end
   end
 
   def destroy
+    image&.destroy
+    render json: { message: 'Image deleted!' }
+  end
+
+  private
+
+  def image_params
+    params.permit(:name, :caption, :url)
+  end
+
+  def image
+    @image ||= Image.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+4.times do |i|
+    Image.create(
+        name: 'Placepuppy',
+        caption: 'This pup will gladly hold your space until you upload your own images.',
+        url: 'https://place-puppy.com/300x300'
+    )
+end
