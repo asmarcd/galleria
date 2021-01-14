d6d76d7af1052f11d47881998d7560c63a161470
Chris Asmar <asmarcd@gmail.com>
2021-01-13 19:46:54 -0800

    controllers and routes in place

---
+++ app/controllers/api/v1/images_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ImagesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'images/index'
+      post 'images/create'
+      get '/show/:id', to: 'images#show'
+      delete '/destroy/:id', to: 'images#destroy'
+    end
+  end
   root 'gallery#index'
+  get '/*path' => 'gallery#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
